d3fefcb7bda616db2fa9ca43ad140e4bd3c8eb1e
adva-mo <advamozes@gmail.com>
2022-10-21 01:59:07 +0300

    random number func, generate div ID func, getElementById func, generate soil func

--- app.js
+++ app.js
@@ -1,31 +1,81 @@
 const grid = document.querySelector("#game-board");
 
-// first.classList.add("red");
+/*
+*
+// --------help functions - tested!
+*
+*/
 
-for (let i = 0; i <= 20; i++) {
-  for (let j = 0; j <= 30; j++) {
-    let newDiv = document.createElement("div");
-    newDiv.setAttribute("id", `${i},${j}`);
-    grid.appendChild(newDiv);
+const randomNumAtoB = (a, b) => {
+  return a + Math.floor(Math.random() * (b + 1 - a));
+};
+
+const generateDivIDs = () => {
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 30; j++) {
+      let newDiv = document.createElement("div");
+      newDiv.setAttribute("id", `(${j},${i})`);
+      grid.appendChild(newDiv);
+    }
   }
+};
+
+const getElementByIdF = (x, y) => {
+  return document.getElementById(`(${x},${y})`);
+};
+
+const generateSoil = () => {
+  let x = randomNumAtoB(13, 18);
+  for (let i = 0; i < 20 - x; i++) {
+    for (let j = 0; j < 30; j++) {
+      let newDiv = getElementByIdF(0 + j, x + i);
+      if (newDiv) {
+        newDiv.classList.add("soil");
+      }
+    }
+  }
+  console.log(x);
+};
+/*
+*
+// --------hgame play
+*
+*/
+
+function generateWorld() {
+  generateDivIDs();
+  generateSoil();
 }
-let first = document.getElementById("5,5");
-// first.classList.add("red");
+
+generateWorld();
+/*
+*
+// --------temporary functions
+*
+*/
 
 function getRandomPosition() {
   return 5, 5;
 }
+
 function getRandomTree() {
   //   let len = Math.floor(Math.random() * 6);
   return 4;
 }
 // getRandomTree();
-function drawElement(x, y, len) {
-  var tree = document.getElementById(`${x},${y}`);
+function drawElement(y) {
+  let len = randomNumAtoB(2, 5);
+
+  let x = randomNumAtoB(0, 30);
+  let tree = document.getElementById(`(${x},${y})`);
+  console.log(tree);
   for (let i = 0; i < len; i++) {
     ++x;
-    tree = document.getElementById(`${x},${y}`);
-    tree.classList.add("red");
+    tree = document.getElementById(`(${x},${y})`);
+    if (tree) {
+      tree.classList.add("red");
+    }
   }
 }
-drawElement(11, 22, 3);
+
+drawElement(17);
